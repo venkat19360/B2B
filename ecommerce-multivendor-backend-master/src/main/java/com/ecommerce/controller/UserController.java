611e14b4ec38d3914c2559aac78c99eba263885e
vinairjay <165387526+vinairjay@users.noreply.github.com>
2024-05-15 18:44:08 +0530

    Update UserController.java

--- ecommerce-multivendor-backend-master/src/main/java/com/ecommerce/controller/UserController.java
+++ ecommerce-multivendor-backend-master/src/main/java/com/ecommerce/controller/UserController.java
@@ -26,7 +26,7 @@ import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("api/user")
-@CrossOrigin(origins = {"http://15.207.180.250:3000/", "http://15.207.180.250:8081/"})
+@CrossOrigin(origins = {"http://15.207.180.250:3000", "http://15.207.180.250:8081"})
 public class UserController {
 
 	@Autowired
